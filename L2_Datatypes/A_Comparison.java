ea37cc81447a962511e0bd522ab0448019ce8739
решение <007506070loy@mail.ru>
2021-10-01 20:03:34 +0300

    решение

--- L2_Datatypes/A_Comparison.java
+++ L2_Datatypes/A_Comparison.java
@@ -3,5 +3,14 @@ package L2_Datatypes;
 public class A_Comparison {
     public static void main(String[] args) {
 
+        double a = 1.3;
+        double b = 1.4;
+
+        if(a==b){
+            System.out.println("true");
+        }
+        if(a!=b){
+            System.out.println("false");
+        }
     }
 }
